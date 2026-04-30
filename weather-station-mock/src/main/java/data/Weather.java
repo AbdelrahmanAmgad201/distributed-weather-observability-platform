f78df74cc6bd1792b227e57d2171c848229c998a
AbdelRahman Amgad <abdelrahmanamgad04@gmail.com>
2026-04-30 21:24:23 +0300

    Done station-mock and kafka connection

--- weather-station-mock/src/main/java/data/Weather.java
+++ weather-station-mock/src/main/java/data/Weather.java
@@ -5,5 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record Weather(
         Integer humidity,
         Integer temperature,
-        @JsonProperty("wind speed") Integer windSpeed
+        @JsonProperty("wind_speed") Integer windSpeed
 ) {}
